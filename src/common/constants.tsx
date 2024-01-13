1887ae850d9cf081c19e5ff1c947a8a0603a6ff0
Naresh V <nareshvenkat14@gmail.com>
2024-01-13 23:28:38 +0530

    constants update

--- src/common/constants.tsx
+++ src/common/constants.tsx
@@ -10,6 +10,14 @@ export const GROUP_TYPES: GroupType[] =[
     {label: 'Couple', icon: <FavoriteBorderIcon/>},
 ]
 
+export const SPLIT_METHOD = {
+    EQUALLY: 'Equally',
+    UNEQUALLY: 'Unequally',
+    BY_PERCENTAGE: 'By Percentage'
+}
+
+export const BASE_URL: string = 'https://dead-2409-40f4-100c-e2b5-1c29-82ca-6616-5e9c.ngrok-free.app';
+
 // Temp data
 const FRIENDS: Friend[] = [
     {name: 'Naresh', email: 'xyz@gmail.com', userId: 1},
@@ -24,15 +32,15 @@ export const GROUPS: Group[] = [
     {groupId: 123, name : 'Vasanth S', description: 'random'},
     {groupId: 145, name : 'Balaji S', description: 'Random'},
     {groupId: 155, name : 'Naresh Vegfgfvnkat 2', description: 'random'},
-    {groupId: 144, name : 'Balaji S', description: 'Random'},
-    {groupId: 167, name : 'Karthic Kumar 3', description: 'Random'},
-    {groupId: 166, name : 'Balaji S', description: 'Random'},
+    {groupId: 144, name : 'Balaji S2', description: 'Random'},
+    {groupId: 167, name : 'Karthic Kumar 2', description: 'Random'},
+    {groupId: 166, name : 'Balaji S3', description: 'Random'},
     {groupId: 177, name : 'Karthic Kumar 3', description: 'Random'},
-    {groupId: 18, name : 'Balaji S', description: 'Random'},
-    { groupId: 39, name : 'Karthic Kumar 3', description: 'Random'},
+    {groupId: 18, name : 'Balaji S4', description: 'Random'},
+    {groupId: 39, name : 'Karthic Kumar 34', description: 'Random'},
 ];
 
-export const GROUP_FRIENDS_OPTIONS: GroupFriendOption[] = [...FRIENDS, ...GROUPS].map((option: GroupFriend) => {
+export const GROUP_FRIENDS_OPTIONS: GroupFriendOption[] = [...GROUPS].map((option: GroupFriend) => {
     return {
         category: !!option.groupId ? 'Groups' : 'Friends',
         label: option.name,
